383f6d60b7f75e17b5db532221f785028163f6be
Tejas <tejasnaikj9@gmail.com>
2025-03-18 09:38:17 +0530

    First commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, World--2\n";
+  cout << "Hello, Jenkins--2\n";
+  cout << "I have successfully built and run this --2 \n";
   return 0;
 }
